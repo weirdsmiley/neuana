c0e599d41350b7cd1b94ce97107017e63e33785b
Manas <manas18244@iiitd.ac.in>
2022-10-13 16:54:12 +0530

    Remove cluttering debug printfs

--- lib/Analysis.cpp
+++ lib/Analysis.cpp
@@ -21,8 +21,6 @@ auto absolute_error<Vec>(const Vec &true_val, const Vec &approx_val) -> Vec {
 template <>
 auto absolute_error<Matrix>(const Matrix &true_val, const Matrix &approx_val)
     -> Matrix {
-  std::cout
-      << "Reached absolute_error function for vector<vector<double>> ...\n";
   assert(true_val.size() == approx_val.size() &&
          true_val.begin()->size() == approx_val.begin()->size());
 
@@ -33,7 +31,6 @@ auto absolute_error<Matrix>(const Matrix &true_val, const Matrix &approx_val)
 
 template <>
 auto relative_error<Vec>(const Vec &true_val, const Vec &approx_val) -> Vec {
-  std::cout << "Reached relative_error function for vector<double> ...\n";
   assert(true_val.size() == approx_val.size());
 
   Vec err;
@@ -49,8 +46,6 @@ auto relative_error<Vec>(const Vec &true_val, const Vec &approx_val) -> Vec {
 template <>
 auto relative_error<Matrix>(const Matrix &true_val, const Matrix &approx_val)
     -> Matrix {
-  std::cout
-      << "Reached absolute_error function for vector<vector<double>> ...\n";
   assert(true_val.size() == approx_val.size() &&
          true_val.begin()->size() == approx_val.begin()->size());
 
@@ -58,13 +53,5 @@ auto relative_error<Matrix>(const Matrix &true_val, const Matrix &approx_val)
   typedef Matrix::const_iterator MIter;
   return err;
 }
-
-// using mytype = double**;
-// template <>
-// inline auto absolute_error<mytype>(const mytype &true_val, const mytype
-// &approx_val) {
-//   std::cout << "Reached absolute_error function for double** ...\n";
-//   return true_val;
-// }
 
 }; // namespace analysis
